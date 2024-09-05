60cccc5bef7274225ed3afe60fb5ad5993097468
Tabor Roderiques <taborroderiques@yahoo.com>
2024-09-05 08:48:47 -0400

    corrected gtf2bed.py to look for gene name and not gene id

--- d2-homework/gtf2bed.py
+++ d2-homework/gtf2bed.py
@@ -9,21 +9,24 @@ for line in file:
     if "##" in line:
         continue
     fields = line.split("\t")
-  
-    text = fields[8]
+    if fields[2]== "gene":
+
+        text = fields[8]
     
-    start_delimiter = 'gene_id "'
-    end_delimiter = '"'
+        start_delimiter = 'gene_name "'
+        end_delimiter = '"'
 
-    # Find the start index of the gene_id value
-    start_index = text.find(start_delimiter) + len(start_delimiter)
+        # Find the start index of the gene_name value
+        start_index = text.find(start_delimiter) + len(start_delimiter)
 
-    # Find the end index of the gene_id value
-    end_index = text.find(end_delimiter, start_index)
+        # Find the end index of the gene_name value
+        end_index = text.find(end_delimiter, start_index)
 
-    # Extract the substring between the delimiters
-    extracted_value = text[start_index:end_index]
+        # Extract the substring between the delimiters
+        extracted_value = text[start_index:end_index]
 
-    print(str(fields[0]) + '\t' + str(fields[3]) + '\t' + str(fields[4]) + '\t' + extracted_value)
+        print(str(fields[0]) + '\t' + str(fields[3]) + '\t' + str(fields[4]) + '\t' + extracted_value)
+    else:
+        continue
 
 file.close()
