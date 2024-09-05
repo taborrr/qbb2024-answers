da101e5597e0891a1a89a9ffca90f8088b59c446
Tabor Roderiques <taborroderiques@yahoo.com>
2024-09-05 09:08:13 -0400

    just for fun inverse grep addition

--- d2-homework/grep.py
+++ d2-homework/grep.py
@@ -5,15 +5,22 @@ import sys
 # to use this basic grep program run in the directory with this .py file:
 # ./grep.py <grep word> <file to grep> 
 
-file = open( sys.argv[2] )
-grep = sys.argv[1]
 
+# optional inverse grep -v function
+if sys.argv[1] == "-v":
+    grep = sys.argv[2]
+    file = open( sys.argv[3] )
+    for line in file:
+        line = line.rstrip("\n")
+        if grep not in line:
+            print(line)
 
-for line in file:
-    line = line.rstrip("\n")
-    if "##" in line:
-        continue
-    if grep in line:
-        print(line)
+if sys.argv[1] != "-v":
+    grep = sys.argv[1]
+    file = open( sys.argv[2] )
+    for line in file:
+        line = line.rstrip("\n")
+        if grep in line:
+            print(line)
 
 file.close()
