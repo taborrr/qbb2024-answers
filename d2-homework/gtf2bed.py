93f36d697598ab9840383ebeedbb24d0a1b6d2c4
Tabor Roderiques <taborroderiques@yahoo.com>
2024-09-12 19:28:07 -0400

    corrected methods for string extraction

--- d2-homework/gtf2bed.py
+++ d2-homework/gtf2bed.py
@@ -12,20 +12,24 @@ for line in file:
     if fields[2]== "gene":
 
         text = fields[8]
-    
-        start_delimiter = 'gene_name "'
-        end_delimiter = '"'
+        get_genename = text.split(' ')
+        gname = get_genename[5]
+        extracted_string = gname.strip('";')
+        
+        ## method below did not use fields approach
+        # start_delimiter = 'gene_name "'
+        # end_delimiter = '"'
 
-        # Find the start index of the gene_name value
-        start_index = text.find(start_delimiter) + len(start_delimiter)
+        # # Find the start index of the gene_name value
+        # start_index = text.find(start_delimiter) + len(start_delimiter)
 
-        # Find the end index of the gene_name value
-        end_index = text.find(end_delimiter, start_index)
+        # # Find the end index of the gene_name value
+        # end_index = text.find(end_delimiter, start_index)
 
-        # Extract the substring between the delimiters
-        extracted_value = text[start_index:end_index]
+        # # Extract the substring between the delimiters
+        # extracted_value = text[start_index:end_index]
 
-        print(str(fields[0]) + '\t' + str(fields[3]) + '\t' + str(fields[4]) + '\t' + extracted_value)
+        print(str(fields[0]) + '\t' + str(fields[3]) + '\t' + str(fields[4]) + '\t' + extracted_string)# extracted_value) 
     else:
         continue
 
