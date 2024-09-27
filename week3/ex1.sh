6146da3ab1e6f6964ff568925986043fe740c42f
Tabor Roderiques <taborroderiques@yahoo.com>
2024-09-27 12:09:19 -0400

    initial push for creation of the ex1 file

---
+++ week3/ex1.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+### Question 1.1 ###
+
+
+my unix commands | to perform | Q1.1 analysis here
+# A written answer to Question 1.1 based on the output of my code above can go here here.
+
+
+### Question 1.2 ###
+
+my unix commands | to perform | Q1.2 analysis here
+# A written answer to Question 1.2 based on the output of my code above can go here here.
